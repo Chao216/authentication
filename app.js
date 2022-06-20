d955499a12b565ea0c27fe4565bdf37a02c910ca
Chao216 <co.jiaan@proton.me>
2022-06-20 14:25:55 +0300

    build the skeleton

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 //jshint esversion:6
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req,res)=>{
+  console.log("server started on port"+port);
+})
