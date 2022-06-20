6a10b1c97dc452459794bf53b7c547e66e2724f5
Chao216 <co.jiaan@proton.me>
2022-06-20 14:33:55 +0300

    commit and push  from shell script

--- app.js
+++ app.js
@@ -6,29 +6,24 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
+//create a get route
 
+app.get("/", (req,res)=>{
+  res.render("home")
+})
 
+app.get("/login", (req,res)=>{
+  res.render("login")
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/register", (req,res)=>{
+  res.render("register")
+})
 
 
 
@@ -37,6 +32,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, (req,res)=>{
-  console.log("server started on port"+port);
+app.listen(port, (req, res) => {
+  console.log("server started on port" + port);
 })
